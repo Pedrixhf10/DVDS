4a48bd99093656c7e91b13b71c20ec7a73ac29be
Pedrixhf10 <163464505+Pedrixhf10@users.noreply.github.com>
2024-03-14 17:42:49 -0300

    Add files via upload
    Alterações efetuadas no dia 14/3

---
+++ footer.php
@@ -0,0 +1,9 @@
+    </div>
+
+    <!-- Container que abriga o rodapé da página -->
+    <div class="container-fluid text-center bg-dark text-light fixed-bottom">
+        <p>Sistema Web desenvolvimento por Pedrix</p>
+    </div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include("header.php") ?>
+
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+    <p>Aqui ficara o conteudo da pagina</p>
+
+<?php include("footer.php") ?>
+
